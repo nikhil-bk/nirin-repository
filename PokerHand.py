d46dc56a5dac085e933d6fc5db6d8d540073b205
nikhil-bk <nikhilbk9148@gmail.com>
2024-02-20 21:31:41 +0530

    solved 1,2,3,4

--- PokerHand.py
+++ PokerHand.py
@@ -51,20 +51,9 @@ class ThreeCardPokerHand(Hand):
 
     def is_three_of_a_kind(self):
         return len(set(self.ranks)) == 1
-    
-    @staticmethod
-    def rank_to_value(rank):
-        """
-        Convert card rank to numerical value for straight evaluation.
-        """
-        if rank in Card.ranks[:-1]:  # Exclude 'Ace' from numerical comparison
-            return Card.ranks.index(rank)
-        else:
-            return Card.ranks.index('Ace')
+
     def is_straight(self):
-        rank_values = [self.rank_to_value(rank) for rank in self.ranks]
-        return max(rank_values) - min(rank_values) == 2 and len(set(rank_values)) == 3
-        # return max(self.ranks) - min(self.ranks) == 2 and len(set(self.ranks)) == 3
+        return max(self.ranks) - min(self.ranks) == 2 and len(set(self.ranks)) == 3
 
     def is_flush(self):
         return len(set(self.suits)) == 1
@@ -72,7 +61,7 @@ class ThreeCardPokerHand(Hand):
     def is_pair(self):
         return len(set(self.ranks)) == 2
 
-    # def _compare(self, other):
+    def _compare(self, other):
         """
         Compare this hand with another hand according to the game rules.
         Return -1 if other is winning, 0 if tied, and 1 if self is winning.
@@ -96,55 +85,19 @@ class ThreeCardPokerHand(Hand):
             elif self.rank == 1:  # Pair
                 # Compare the ranks of the paired cards first
                 return 1 if self._compare_pairs(other.ranks) else 0
-    def _compare(self, other):
-        if self.rank != other.rank:
-            return 1 if self.rank > other.rank else -1
-        else:
-            # Same rank, use tie-breaking rules
-            if self.rank == 5:  # Straight Flush or Straight
-                # Compare the highest ranks of both hands
-                return 1 if self.rank_to_value(self.ranks[0]) > self.rank_to_value(other.ranks[0]) else \
-                       -1 if self.rank_to_value(self.ranks[0]) < self.rank_to_value(other.ranks[0]) else 0
-            elif self.rank == 4:  # Three of a Kind
-                # Compare the rank of an arbitrary card in both hands
-                return 1 if self.rank_to_value(self.ranks[0]) > self.rank_to_value(other.ranks[0]) else \
-                       -1 if self.rank_to_value(self.ranks[0]) < self.rank_to_value(other.ranks[0]) else 0
-            elif self.rank == 3:  # Straight
-                # Compare the highest ranks of both hands
-                return 1 if self.rank_to_value(self.ranks[0]) > self.rank_to_value(other.ranks[0]) else \
-                       -1 if self.rank_to_value(self.ranks[0]) < self.rank_to_value(other.ranks[0]) else 0
-            elif self.rank == 2 or self.rank == 0:  # Flush or Nothing
-                # Compare the ranks in lexicographical order
-                return self._compare_lexicographical(self.ranks, other.ranks)
-            elif self.rank == 1:  # Pair
-                # Compare the ranks of the paired cards first
-                return 1 if self._compare_pairs(self.ranks, other.ranks) else \
-                       -1 if self._compare_pairs(other.ranks, self.ranks) else 0
     
-    # def _compare_lexicographical(self, other_ranks):
-    #     """
-    #     Compare ranks lexicographically.
-    #     Return 1 if self is winning, -1 if other is winning, and 0 if tied.
-    #     """
-    #     for self_rank, other_rank in zip(self.ranks, other_ranks):
-    #         if self_rank > other_rank:
-    #             return 1
-    #         elif self_rank < other_rank:
-    #             return -1
-    #     return 0
     
-    def _compare_lexicographical(self, self_ranks, other_ranks):
+    def _compare_lexicographical(self, other_ranks):
         """
         Compare ranks lexicographically.
         Return 1 if self is winning, -1 if other is winning, and 0 if tied.
         """
-        for self_rank, other_rank in zip(self_ranks, other_ranks):
-            if self.rank_to_value(self_rank) > self.rank_to_value(other_rank):
+        for self_rank, other_rank in zip(self.ranks, other_ranks):
+            if self_rank > other_rank:
                 return 1
-            elif self.rank_to_value(self_rank) < self.rank_to_value(other_rank):
+            elif self_rank < other_rank:
                 return -1
         return 0
-    
     def _compare_pairs(self, other_ranks):
         """
         Compare ranks of paired cards.
